6bd9ab618d677279dd6d5262cdea094aaf523d16
liuhuaxiang <liuhuaxiang5@huaqin.com>
2021-08-27 11:42:08 +0800

    添加项目文件。

---
+++ ShakeMouseNET/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ShakeMouseNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainNotifyIcon
+            // 
+            this.mainNotifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.mainNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("mainNotifyIcon.Icon")));
+            this.mainNotifyIcon.Text = "ShakeMouseNET";
+            this.mainNotifyIcon.Visible = true;
+            this.mainNotifyIcon.Click += new System.EventHandler(this.mainNotifyIcon_Click);
+            this.mainNotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mainNotifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem,
+            this.关闭ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.关于ToolStripMenuItem.Text = "关于(A)";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.关闭ToolStripMenuItem.Text = "关闭(E)";
+            this.关闭ToolStripMenuItem.Click += new System.EventHandler(this.关闭ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "功能：保持系统处于活动状态；\r\n使用：左键启动/暂停功能；\r\n/*[确定]访问作者网站*/\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 87);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ShakeMouseNET";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon mainNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ShakeMouseNET/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ShakeMouseNET
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_LBUTTONDOWN = 513; // 鼠标左键按下  
+        public const int WM_LBUTTONUP = 514; // 鼠标左键抬起  
+        public const int WM_RBUTTONDOWN = 516; // 鼠标右键按下  
+        public const int WM_RBUTTONUP = 517; // 鼠标右键抬起  
+        public const int WM_MBUTTONDOWN = 519; // 鼠标中键按下  
+        public const int WM_MBUTTONUP = 520; // 鼠标中键抬起  
+
+        public const int MOUSEEVENTF_MOVE = 0x0001; // 移动鼠标         
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002; // 鼠标左键按下        
+        public const int MOUSEEVENTF_LEFTUP = 0x0004; // 鼠标左键抬起        
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x0008; // 鼠标右键按下       
+        public const int MOUSEEVENTF_RIGHTUP = 0x0010; // 鼠标右键抬起          
+        public const int MOUSEEVENTF_MIDDLEDOWN = 0x0020; // 鼠标中键按下    
+        public const int MOUSEEVENTF_MIDDLEUP = 0x0040; // 鼠标中键抬起           
+        public const int MOUSEEVENTF_ABSOLUTE = 0x8000; // 绝对坐标 
+
+        public static bool start = true;
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr WindowFromPoint(int xPoint, int yPoint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ScreenToClient(IntPtr hWnd, ref Point lppt);
+
+        // [DllImport("user32", SetLastError = true)]  
+        // public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("user32", SetLastError = true)]  
+        public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+        // 选项所用到的常数
+        const uint ES_AWAYMODE_REQUIRED = 0x00000040;
+        const uint ES_CONTINUOUS = 0x80000000;
+        const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.Visible = false;
+            this.ShowInTaskbar = false;
+
+            this.WindowState = FormWindowState.Minimized;
+            this.mainNotifyIcon.Visible = true;
+            this.Hide();
+            Thread thread = new Thread(new ThreadStart(ThreadMouse));
+            thread.Start();
+
+        }
+
+        static void ThreadMouse()
+        {
+            /*
+              这个没什么东西的，主要就这一句：
+            Mouse_Event(MOUSEEVENTF_MOVE,0,0,0,0); 
+            也可以用这个：SetThreadExecutionState(ES_CONTINUOUS or ES_SYSTEM_REQUIRED or ES_DISPLAY_REQUIRED) ;
+            兼容性应该更好一些。
+            */
+
+            while (true)
+            {
+                if (start)
+                { 
+                    mouse_event(MOUSEEVENTF_MOVE, 0, 0, 0, 0);
+                    SetThreadExecutionState(ES_CONTINUOUS);
+                }
+                Thread.Sleep(30*1000);
+            }
+        }
+
+        private void mainNotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.mainNotifyIcon.Visible = true;
+                this.Hide();
+            }
+            else
+            {
+                this.Visible = true;
+                this.WindowState = FormWindowState.Normal;
+                this.Activate();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 注意判断关闭事件reason来源于窗体按钮，否则用菜单退出时无法退出!
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                //取消"关闭窗口"事件
+                e.Cancel = true; // 取消关闭窗体 
+
+                //使关闭时窗口向右下角缩小的效果
+                this.WindowState = FormWindowState.Minimized;
+                this.mainNotifyIcon.Visible = true;
+                this.Hide();
+                return;
+            }
+        }
+
+        private void 关闭ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("你确定要退出？", "系统提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+
+                this.mainNotifyIcon.Visible = false;
+                this.Close();
+                this.Dispose();
+                System.Environment.Exit(System.Environment.ExitCode);
+
+            }
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.mainNotifyIcon.Visible = true;
+            this.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://cfblog.blackduck.workers.dev/");
+        }
+
+        private void mainNotifyIcon_Click(object sender, EventArgs e)
+        {
+
+            if (start)
+            { 
+                start = false;
+                this.mainNotifyIcon.Icon= Properties.Resources._2;
+                this.mainNotifyIcon.Text = "已经停止";
+            }
+            else
+            {
+                start = true;
+                this.mainNotifyIcon.Icon = Properties.Resources._1;
+                this.mainNotifyIcon.Text = "正在运行";
+            }
+               
+        }
+    }
+}
